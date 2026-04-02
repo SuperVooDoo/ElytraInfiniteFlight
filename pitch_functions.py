8a2df28fd3aaa6ef42527894644122d9047281ea
Morgan Aubert <morganaubert425@gmail.com>
2026-04-02 18:14:52 +0900

    Renamed logic --> strategy

--- pitch_functions.py
+++ pitch_functions.py
@@ -11,3 +11,33 @@ def Rectangles_pitch_function(descent_time = 160, descent_angle = 33, ascent_tim
         np.full(int(descent_time), descent_angle),
         np.full(int(ascent_time), ascent_angle)
     ])
+
+def Waves_pitch_function(descent_time = 120, descent_angle = 33, up_angle = -50, ascent_pitch_rate = 0.5, ascent_time = 120, ascent_angle = -33, down_angle= 50, descent_pitch_rate = -0.5):
+    descent_phase = np.concatenate([
+        np.full(int(descent_time), descent_angle),
+        np.arange(up_angle, ascent_angle, ascent_pitch_rate)
+    ])
+    ascent_phase = np.concatenate([
+        np.full(int(ascent_time), ascent_angle),
+        np.arange(down_angle, descent_angle, descent_pitch_rate)
+    ])
+    return np.concatenate([descent_phase, ascent_phase])
+
+
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    def DisplayFunction(function, N_patterns = 3):
+
+        Y = np.tile(function(), N_patterns)
+
+        plt.plot(Y, label=function.__name__)
+        plt.xlabel("Time Steps")
+        plt.ylabel("Pitch (degrees)")
+        plt.title(f"Pitch Function: {function.__name__}")
+        plt.legend()
+        plt.show()
+    
+    DisplayFunction(Waves_pitch_function)
